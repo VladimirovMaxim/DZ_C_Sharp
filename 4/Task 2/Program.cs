c9e813fcfe23ab5ce954fd8d79490c7311a370fa
maxim_vladimirov <maxvladimimrov@gmail.com>
2022-07-01 23:36:54 +0300

    Homework

---
+++ 4/Task 2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+void CreateShowArray(int []array , int min, int max)
+{
+    max = max +1;
+    int size=array.Length - 1;
+    Console.Write("[");
+    for(int i = 0;i<size; i++ )
+    {
+        array[i] = new Random().Next(min, max);
+        Console.Write(array[i] + ", ");
+    }
+    array[size] = new Random().Next(min, max);
+    Console.Write(array[size] + "]");
+
+}
+int[] array = new int[8];
+
+Console.WriteLine("Введите максимальное значение в массиве ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимальное значение в массиве ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+CreateShowArray(array ,  min,  max);
