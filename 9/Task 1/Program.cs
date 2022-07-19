0eccb4c396e6731be8156d30199e69022255ca8f
maxim_vladimirov <maxvladimimrov@gmail.com>
2022-07-20 00:12:23 +0300

    Dz_9

---
+++ 9/Task 1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая найдет факториал числа N рекурсивным методом.
+
+// N = 4 -> 24
+
+int Factorial(int n)
+{
+    if (n>0)
+    {
+        return n * Factorial(n-1);
+    }
+    return 1;
+}
+
+
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($" Факториал {n} равен {Factorial(n)}");
